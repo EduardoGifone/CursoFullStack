1d9e8a145de04db1ebd7b02247dc308bbc5bc6b9
EduardoGifone <mangdulce@gmail.com>
2024-04-30 17:16:12 -0500

    CRUD terminado

--- bienesraices_inicio/includes/templates/anuncios.php
+++ bienesraices_inicio/includes/templates/anuncios.php
@@ -1,49 +1,30 @@
 <?php 
-
-    $id = $_GET["id"];
-    $id = filter_var($id, FILTER_VALIDATE_INT);
-    // var_dump($id);
-    if (!$id) header("Location: /index.php");
-
     // Importar conexion
     require "includes/config/database.php";
     $db = conectarDB();
 
     // Crear consulta
-    $query = "SELECT * FROM propiedades WHERE id = $id";
+
+    $limite = $limite ?? "";
+
+    $query = "SELECT * FROM propiedades " . $limite;
 
     // Obtener datos
-    $resultado = mysqli_query($db, $query);
-    
-    if(!$resultado->num_rows)
-        header("location: /");
-    
-    // SOLUCION PARA ID QUE NO EXISTEN EN LA BASE DE DATOS
-    
-    // if(mysqli_num_rows($resultado) > 0) {
-        //     $propiedad = mysqli_fetch_assoc($resultado);
-        // } else {
-    //     // Manejar el caso en el que no se encontró ninguna propiedad con el ID dado
-    //     // echo "La propiedad con el ID $id no fue encontrada.";
-    //     header("Location: /index.php");
-    // }
-    
-    $propiedad = mysqli_fetch_assoc($resultado);
-    
-    require "includes/funciones.php";
 
-    incluirTemplate("header");
+    $resultado = mysqli_query($db, $query);
+
 ?>
 
 
-    <main class="contenedor seccion contenido-centrado">
-        <h1><?php echo $propiedad["titulo"] ;?></h1>
-
+<div class="contenedor-anuncios">
+    <?php while($propiedad = mysqli_fetch_assoc($resultado)) {?>
+    <div class="anuncio">
         <img loading="laxy" width="200" height="300" src="imagenes/<?php echo $propiedad["imagen"] ;?>" alt="Anuncio">
 
-        <div class="resumen-propiedad">
+        <div class="contenido-anuncio">
+            <h3><?php echo $propiedad["titulo"] ;?></h3>
+            <p><?php echo $propiedad["descripcion"] ;?></p>
             <p class="precio">$<?php echo $propiedad["precio"] ;?></p>
-
             <ul class="iconos-caracteristicas">
                 <li>
                     <img class="icono" src="build/img/icono_wc.svg" alt="Icono wc" loading="lazy">
@@ -59,14 +40,15 @@
                 </li>
             </ul>
 
-            <p><?php echo $propiedad["descripcion"] ;?></p>
-
-        </div>
-    </main>
+            <a href="anuncio.php?id=<?php echo $propiedad["id"]; ?>" class="boton-amarillo-block">
+                Ver Propiedad
+            </a>
+        </div><!--.contenido-anuncio-->
+    </div><!--.anuncio-->
+    <?php } ?>
+</div><!--.contenedor-anuncios-->
 
 <?php 
-    incluirTemplate("footer");
-
     // Cerrar conexion
     mysqli_close($db);
 ?>
--- bienesraices_inicio/index.php
+++ bienesraices_inicio/index.php
@@ -31,105 +31,14 @@
     <section class="seccion contenedor">
         <h2>Casas y Depas en Venta</h2>
 
-        <div class="contenedor-anuncios">
-            <div class="anuncio">
-                <picture>
-                    <source srcset="build/img/anuncio1.webp" type="image/webp">
-                    <img loading="laxy" width="200" height="300" src="build/imb/anuncio1.jpg" alt="Anuncio">
-                </picture>
-
-                <div class="contenido-anuncio">
-                    <h3>Casa de Lujo en el Lago</h3>
-                    <p>Casa en el lago con excelente vista, acabados de lujo a un excelente precio</p>
-                    <p class="precio">$3.000.000</p>
-                    <ul class="iconos-caracteristicas">
-                        <li>
-                            <img class="icono" src="build/img/icono_wc.svg" alt="Icono wc" loading="lazy">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" src="build/img/icono_estacionamiento.svg" alt="Icono icono_estacionamiento" loading="lazy">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" src="build/img/icono_dormitorio.svg" alt="Icono habitaciones" loading="lazy">
-                            <p>4</p>
-                        </li>
-                    </ul>
-
-                    <a href="anuncio.php" class="boton-amarillo-block">
-                        Ver Propiedad
-                    </a>
-                </div><!--.contenido-anuncio-->
-            </div><!--.anuncio-->
-
-            <div class="anuncio">
-                <picture>
-                    <source srcset="build/img/anuncio2.webp" type="image/webp">
-                    <img loading="laxy" width="200" height="300" src="build/imb/anuncio2.jpg" alt="Anuncio">
-                </picture>
-
-                <div class="contenido-anuncio">
-                    <h3>Casa terminados de Lujo</h3>
-                    <p>Casa en el lago con excelente vista, acabados de lujo a un excelente precio</p>
-                    <p class="precio">$3.000.000</p>
-                    <ul class="iconos-caracteristicas">
-                        <li>
-                            <img class="icono" src="build/img/icono_wc.svg" alt="Icono wc" loading="lazy">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" src="build/img/icono_estacionamiento.svg" alt="Icono icono_estacionamiento" loading="lazy">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" src="build/img/icono_dormitorio.svg" alt="Icono habitaciones" loading="lazy">
-                            <p>4</p>
-                        </li>
-                    </ul>
-
-                    <a href="anuncio.php" class="boton-amarillo-block">
-                        Ver Propiedad
-                    </a>
-                </div><!--.contenido-anuncio-->
-            </div><!--.anuncio-->
-
-            <div class="anuncio">
-                <picture>
-                    <source srcset="build/img/anuncio3.webp" type="image/webp">
-                    <img loading="laxy" width="200" height="300" src="build/imb/anuncio3.jpg" alt="Anuncio">
-                </picture>
-
-                <div class="contenido-anuncio">
-                    <h3>Casa con alberca</h3>
-                    <p>Casa en el lago con excelente vista, acabados de lujo a un excelente precio</p>
-                    <p class="precio">$3.000.000</p>
-                    <ul class="iconos-caracteristicas">
-                        <li>
-                            <img class="icono" src="build/img/icono_wc.svg" alt="Icono wc" loading="lazy">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" src="build/img/icono_estacionamiento.svg" alt="Icono icono_estacionamiento" loading="lazy">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" src="build/img/icono_dormitorio.svg" alt="Icono habitaciones" loading="lazy">
-                            <p>4</p>
-                        </li>
-                    </ul>
-
-                    <a href="anuncio.php" class="boton-amarillo-block">
-                        Ver Propiedad
-                    </a>
-                </div><!--.contenido-anuncio-->
-            </div><!--.anuncio-->
-        </div><!--.contenedor-anuncios-->
+        <?php 
+            $limite = "LIMIT 3";
+            // incluirTemplate("anuncios"); De esta forma no se mandan las variables
+            include "includes/templates/anuncios.php";
+        ?>
 
         <div class="alinear-derecha">
-            <a href="anuncios.php" class="boton-verde">
-                Ver Todas
-            </a>
+            <a href="anuncios.php" class="boton-verde">Ver Todas</a>
         </div>
     </section>
 
